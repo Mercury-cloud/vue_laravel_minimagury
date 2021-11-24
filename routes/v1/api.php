9549c86ace56d85a7fa851ed5943b938373a96c9
h_koutake <hi110977@gmail.com>
2021-11-24 11:18:01 +0900

    保存処理

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', [AuthController::class, 'test']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
