5cbd559cac139a61d21c1f1add472c84d963bc0d
h_koutake <hi110977@gmail.com>
2021-12-07 10:31:18 +0900

    ユーザー追加削除

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,13 @@ use Illuminate\Support\Facades\Route;
 * 管理画面
 ***********/ 
 
-Route::get('/login',      [AuthController::class, 'login'])->name('manage.login');
-Route::post('/login',      [AuthController::class, 'loginAttempt'])->name('manage.login.attempt');
-Route::get('/manage/logout',      [AuthController::class, 'logout'])->name('manage.logout');
+Route::get('/login',         [AuthController::class, 'login'])->name('manage.login');
+Route::post('/login',        [AuthController::class, 'loginAttempt'])->name('manage.login.attempt');
+Route::get('/manage/logout', [AuthController::class, 'logout'])->name('manage.logout');
 
-Route::get('/manage/user',      [UserController::class, 'index'])->name('manage.user.index');
-
-Route::get('/manage/user/add',      [UserController::class, 'add'])->name('manage.user.add');
-Route::post('/manage/user/add',     [UserController::class, 'store'])->name('manage.user.add.post');
-
-Route::get('/manage/user/edit/{user}',      [UserController::class, 'edit'])->name('manage.user.edit');
-Route::post('/manage/user/edit/{user}',     [UserController::class, 'store'])->name('manage.user.edit.post');
-
-Route::get('/manage/user/delete/{user}',      [UserController::class, 'delete'])->name('manage.user.delete');
+Route::get('/manage/user',              [UserController::class, 'index'])->name('manage.user.index');
+Route::get('/manage/user/add',          [UserController::class, 'add'])->name('manage.user.add');
+Route::post('/manage/user/add',         [UserController::class, 'store'])->name('manage.user.add.post');
+Route::get('/manage/user/edit/{user}',  [UserController::class, 'edit'])->name('manage.user.edit');
+Route::post('/manage/user/edit/{user}', [UserController::class, 'store'])->name('manage.user.edit.post');
+Route::get('/manage/user/delete/{user}',[UserController::class, 'delete'])->name('manage.user.delete');
